354498a5f1488ae98c6b5d92e863c575bb673fe2
brenRodriguez <brendaerodriguez@hotmail.com>
2020-02-16 20:51:45 -0300

    Updates

---
+++ precache-manifest.4e5268afc35f6c58bc4c7b6995ea3029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2e769b4d40cffb80ebab9fdbc07960",
+    "url": "/bren-rodriguez-react/index.html"
+  },
+  {
+    "revision": "f0fd1c6b057d62b9df5e",
+    "url": "/bren-rodriguez-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f9d5694b29d7a253643",
+    "url": "/bren-rodriguez-react/static/js/2.4c9786bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bren-rodriguez-react/static/js/2.4c9786bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0fd1c6b057d62b9df5e",
+    "url": "/bren-rodriguez-react/static/js/main.c59732b6.chunk.js"
+  },
+  {
+    "revision": "8fd3f5dfcd447f8660df",
+    "url": "/bren-rodriguez-react/static/js/runtime-main.ccd67d5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bren-rodriguez-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
